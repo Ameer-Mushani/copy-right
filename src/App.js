f08c309f67a55af0d28172603ddb5c83639bff52
hanswang123456@gmail.com <hanswang123456@gmail.com>
2022-05-21 14:44:19 -0400

    some css added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import { useDropzone } from "react-dropzone"
 import { motion} from "framer-motion"
 import "./App.css"
+import logo from './logo.png';
 
 function App() {
   const [files, setFiles] = useState([])
@@ -28,118 +29,77 @@ function App() {
   ))
 
   return (
-    <motion.div className="App" whileHover={{ backgroundColor: "#808080"}}>
-      <div class = "dropArea" {...getRootProps()}>
-        <input {...getInputProps()} />
-        <p>Drop files here</p>
-      </div>
-      <div>{images}</div>
-    </motion.div>
-  )
-}
-
-export default App
-
-/*
-
-
-
-
-
-import logo from './logo.png';
-import { motion} from "framer-motion"
-import './App.css';
-import{useDropzone} from 'react-dropzone'
-
-
-//react 
-import React, {useState} from 'react';
-import * as ReactDOM from 'react-dom';
-
-//function that takes data from the clipboard and returns the text
-
-async function paste() {
-  const text = await navigator.clipboard.readText();
-  return text;
-}
-
-
-function CreateTile(props){
-  return <motion.div drag dragConstraints={{top: -50,left: -50,right: 600,bottom: 300,}} className = "Tile" >{props.data}</motion.div>
-  
- // return <input  className = "Tile" value = {props.data} onClick={() => {navigator.clipboard.writeText(this.innerHTML)}}></input>
-}
-function CreateFileTile(props){
-  return <motion.div drag dragConstraints={{top: -50,left: -50,right: 600,bottom: 300,}} className = "FileTile" >{props.data}</motion.div>
-  }
-
-
-function App() {
-
-  const [files, setFiles] = useState([])
-
-  const {getRoot, getInputProps} = useDropzone({
-    accept: "images/*",
-    onDrop: (acceptedFiles) ={
-      setFiles(
-        acceptedFiles.map(file) => Object.assign((file), {
-          preview: URL.createObjectURL(file)
-        })
-      )
-    }
-
-  });
-
-  const images = files.map((file) => (
-    <div key = {file.name}>
-      <div>
-        <img src = {file.preview} style = {{width: "200px"} } alt = ""/>
-      </div>
-
-    </div>
-  ))
-    return (
-      <div className='App'>
-        <div {...getRoot()}/>
-        <input {...getInputProps()}/>
-        <p>Drop Files Here</p>
-        
-      <div>{images}</div>
-      </div>
-
-    )
-/*
-  return (
-    <div className="App">
+    //drag and drop
+    <div className="App" >
       <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <div id = "signIn">Sign In:
+        <motion.button  whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}className = "googleSignIn"></motion.button>
+
+        </div>
+      </div>
       </header>
-      <body>
-      <div id = "textDiv">
+
+            <body>
+
+
+              <table>
+              <tr>
+                <td class = "inputSide">        
+                <motion.div class = "dropArea" {...getRootProps()} whileHover={{ backgroundColor: "#808080"}}>
+        <input {...getInputProps()} />
+        Drop files here
+      </motion.div>
+          </td>
+          <td><div id = "fileList">{images}</div></td>
+        </tr>
+            <tr>
+              <td class = "inputSide"><input id = "inputField" placeHolder = "Enter Text" value = ""></input>      <motion.button  whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}className = "Tile">Save</motion.button>
+</td>
+                <td>
+                <div id = "textDiv">
             <CreateTile data = "text item 1"/>
             <CreateTile data = "text item 2"/>
             <CreateTile data = "text item 3"/>
             </div>
-            <div id = "fileDiv">
+                </td>
+              </tr>
+              </table>
 
-            </div>
       </body>
 
+      
+
       <footer>
-      <motion.button onClick = {clearSpace} whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}className = "Tile">Clear All</motion.button>
+      <motion.button  whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}className = "Tile">Clear All</motion.button>
       <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} className = "Tile">History</motion.button>
       </footer>
+
     </div>
-  );
+
+    
+  )
+}
+
+//function that takes data from the clipboard and returns the text
+/*
+async function paste() {
+  const text = await navigator.clipboard.readText();
+  return text;
+}
+*/
+
+function CreateTile(props){
+  return <motion.div hover whileHover={{scale:1.2}} className = "Tile" >{props.data}</motion.div>
   
+ // return <input  className = "Tile" value = {props.data} onClick={() => {navigator.clipboard.writeText(this.innerHTML)}}></input>
 }
-
-function clearSpace(){
-  let parent =  document.getElementById("parentDiv")
-  parent.innerHTML = "";
-
-}
-
+/*
+function CreateFileTile(props){
+  return <motion.div drag dragConstraints={{top: -50,left: -50,right: 600,bottom: 300,}} className = "FileTile" >{props.data}</motion.div>
+  }
+  */
 
 ///event listener for ctrl+v
 document.body.addEventListener('keyup', (e) =>{
@@ -151,4 +111,4 @@ document.body.addEventListener('keyup', (e) =>{
 })
 
 
-export default App;*/
+export default App;
