fe87f1f2b35b4045d8f824aa376b52882064db37
hanswang123456@gmail.com <hanswang123456@gmail.com>
2022-05-21 21:59:03 -0400

    React update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React, { useState } from "react"
-import { useDropzone } from "react-dropzone"
-import { motion} from "framer-motion"
-import "./App.css"
-import logo from './logo.png';
-
+import React, { useState } from "react";
+import { useDropzone } from "react-dropzone";
+import { motion } from "framer-motion";
+import "./App.css";
+import logo from "./logo.png";
 
 //react form
-import List from './components/List'
+import List from "./components/List";
 
 function App() {
-  const [files, setFiles] = useState([])
+  const [files, setFiles] = useState([]);
 
   const { getRootProps, getInputProps } = useDropzone({
     accept: "image/*",
@@ -20,9 +19,9 @@ function App() {
             preview: URL.createObjectURL(file),
           })
         )
-      )
+      );
     },
-  })
+  });
 
   const images = files.map((file) => (
     <div key={file.name}>
@@ -30,58 +29,79 @@ function App() {
         <img src={file.preview} style={{ width: "200px" }} alt="preview" />
       </div>
     </div>
-  ))
+  ));
 
   return (
     //drag and drop
-    <div className="App" >
+    <div className="App">
       <header className="App-header">
-      <div>
-        <img src={logo} className="App-logo" alt="logo" />
-        <div id = "signIn">Sign In:
-        <motion.button  whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}className = "googleSignIn"></motion.button>
-
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <div id="signIn">
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+              className="googleSignIn"
+            >
+              sign In
+            </motion.button>
+          </div>
         </div>
-      </div>
       </header>
-
-            <body>
-
-
-              <table>
-              <tr>
-                <td class = "inputSide">        
-                <motion.div class = "dropArea" {...getRootProps()} whileHover={{ backgroundColor: "#808080"}}>
-        <input {...getInputProps()} />
-        Drop files here
-      </motion.div>
-          </td>
-          <td class = "inputSide">
-          <List/>
-          </td>
-        </tr>
+      <div id="bodyContainer">
+        <table className="textTable">
+          <tbody>
             <tr>
-              <td>           
-              <div id = "fileList">{images}</div>
-</td>
-                <td>
-                </td>
-              </tr>
-              </table>
+              <td id="dropSide">
+                <table id="dropTable">
+                  <tr>
+                    <td>
+                      <motion.div
+                        className="dropArea"
+                        {...getRootProps()}
+                        whileHover={{ backgroundColor: "#808080" }}
+                      >
+                        <input {...getInputProps()} />
+                        Drop files here
+                      </motion.div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      {" "}
+                      <div id="fileList">{images}</div>
+                    </td>
+                  </tr>
+                </table>
+              </td>
+              <td className="inputSide">
+                <div class = "outer"><List /></div>
 
-      </body>
-
-      
+                
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
       <footer>
-      <motion.button  whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}className = "Tile">Clear All</motion.button>
-      <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }} className = "Tile">History</motion.button>
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="Tile"
+        >
+          Clear All
+        </motion.button>
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className="Tile"
+        >
+          History
+        </motion.button>
       </footer>
-
     </div>
-
-    
-  )
+  );
 }
 
 //function that takes data from the clipboard and returns the text
@@ -92,24 +112,22 @@ async function paste() {
 }
 */
 
+/*
 function CreateTile(props){
   return <motion.div hover whileHover={{scale:1.2}} className = "Tile" >{props.data}</motion.div>
  // return <input  className = "Tile" value = {props.data} onClick={() => {navigator.clipboard.writeText(this.innerHTML)}}></input>
 }
-/*
+
 function CreateFileTile(props){
   return <motion.div drag dragConstraints={{top: -50,left: -50,right: 600,bottom: 300,}} className = "FileTile" >{props.data}</motion.div>
   }
   */
 
 ///event listener for ctrl+v
-document.body.addEventListener('keyup', (e) =>{
-  if(e.ctrlKey && e.key ==='v'){
+document.body.addEventListener("keyup", (e) => {
+  if (e.ctrlKey && e.key === "v") {
     console.log("ctrl+v is clicked");
-
-
   }
-})
-
+});
 
 export default App;
